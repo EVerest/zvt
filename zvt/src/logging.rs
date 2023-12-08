f6bcc46410fe93fc026748cf64ca3c87669ae64c
Dima Dorezyuk <ddo@qwello.eu>
2023-12-08 09:35:35 +0100

    wip

--- zvt/src/logging.rs
+++ zvt/src/logging.rs
@@ -5,6 +5,7 @@ use async_trait::async_trait;
 pub trait AsyncReadExt: tokio::io::AsyncReadExt + Unpin + Send {
     async fn read_exact<'a>(&'a mut self, buf: &'a mut [u8]) -> tokio::io::Result<usize> {
         let res = <Self as tokio::io::AsyncReadExt>::read_exact(self, buf).await;
+        log::debug!("The length is {} and the result is {:?}", buf.len(), res);
         log::debug!("Read {:?}", buf);
         res
     }
@@ -16,7 +17,7 @@ impl<R: tokio::io::AsyncReadExt + ?Sized + Unpin + Send> AsyncReadExt for R {}
 #[async_trait]
 pub trait AsyncWriteExt: tokio::io::AsyncWriteExt + Unpin + Send {
     async fn write_all<'a>(&'a mut self, src: &'a [u8]) -> tokio::io::Result<()> {
-        log::debug!("Writing {src:?}");
+        log::debug!("Write {src:?}");
         <Self as tokio::io::AsyncWriteExt>::write_all(self, src).await
     }
 }
